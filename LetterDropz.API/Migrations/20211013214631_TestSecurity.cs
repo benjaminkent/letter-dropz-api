bb37beaa5019b8b9863e3169161fd5da04355314
benjaminkent <benjamin.k.jehl@gmail.com>
2021-10-13 17:54:56 -0400

    update connection string and gitignore

---
+++ LetterDropz.API/Migrations/20211013214631_TestSecurity.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LetterDropz.API.Migrations
+{
+    public partial class TestSecurity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Test",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Test",
+                table: "Users");
+        }
+    }
+}
---
+++ LetterDropz.API/Migrations/20211013215019_RemoveTestAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LetterDropz.API.Migrations
+{
+    public partial class RemoveTestAttribute : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Test",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Test",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
